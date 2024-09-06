1d3ddd98317ea940ca5c484aac2b3cf4c3e0afa1
ikram-zerrouki <ki_zerrouki@esi.dz>
2024-09-06 20:32:24 +0100

    Initial commit of Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home';
+import Login from './components/LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Login />
     </div>
   );
 }
