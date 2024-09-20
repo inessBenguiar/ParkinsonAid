5dee4c574cb068da5a2442aed69eb969403a9113
inessBenguiar <109390234+inessBenguiar@users.noreply.github.com>
2024-09-20 10:19:03 +0200

    Update models.py

--- backend/parkinson_aid/models.py
+++ backend/parkinson_aid/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+# DOCTOR MODEL
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100) 
+    phone_number = models.CharField(max_length=15)
+    picture = models.ImageField(upload_to='doctor_pics/', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.email
 
 # PATIENT MODEL
 class Patient(models.Model):
@@ -8,13 +19,14 @@ class Patient(models.Model):
         ('F', 'Female'),
     ]
 
+    doctor = models.ForeignKey('Doctor', on_delete=models.CASCADE, null=True, blank=True, related_name='patients')  # Link to Doctor
     name = models.CharField(max_length=100)
     patient_id = models.CharField(max_length=10, unique=True)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     phone_number = models.CharField(max_length=15)
     email = models.EmailField()
-    diagnosis_result = models.CharField(max_length=200)
+    diagnosis_result = models.CharField(max_length=200)  # ??????????????????????????
     description = models.TextField()
     date_of_last_update = models.DateField(auto_now=True)
     doctor_name = models.CharField(max_length=100)
