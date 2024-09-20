21fe6a4ad49a085c029a35b6b6d4a942e4550949
inessBenguiar <109390234+inessBenguiar@users.noreply.github.com>
2024-09-20 10:18:15 +0200

    Update admin.py

--- backend/parkinson_aid/admin.py
+++ backend/parkinson_aid/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Patient, MRIImage
+from .models import Patient, MRIImage, Doctor 
 
 # Register your models here.
 admin.site.register(Patient)
 admin.site.register(MRIImage)
+admin.site.register(Doctor)  
